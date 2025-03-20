728cb8d49a704ab03d88f743a527745d74f85f84
Larsbuilds <larstischer@icloud.com>
2025-03-20 16:21:32 +0200

    refactor: move client components to wrapper

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,24 +1,6 @@
-"use client"
-
 import React from 'react'
-import { HeroSection } from "@/components/sections/HeroSection"
-import { FeaturesSection } from "@/components/sections/FeaturesSection"
-import { PricingSection } from "@/components/sections/PricingSection"
-import { AboutSection } from "@/components/sections/AboutSection"
-import { TestimonialsSection } from "@/components/sections/TestimonialsSection"
-import { Footer } from "@/components/layout/Footer"
+import { ClientWrapper } from "@/components/layout/ClientWrapper"
 
 export default function Home() {
-  return (
-    <>
-      <main className="flex-1">
-        <HeroSection />
-        <FeaturesSection />
-        <AboutSection />
-        <TestimonialsSection />
-        <PricingSection />
-      </main>
-      <Footer />
-    </>
-  )
+  return <ClientWrapper />
 } 
---
+++ app/components/layout/ClientWrapper.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+import { HeroSection } from "@/components/sections/HeroSection"
+import { FeaturesSection } from "@/components/sections/FeaturesSection"
+import { PricingSection } from "@/components/sections/PricingSection"
+import { AboutSection } from "@/components/sections/AboutSection"
+import { TestimonialsSection } from "@/components/sections/TestimonialsSection"
+import { Footer } from "@/components/layout/Footer"
+
+export function ClientWrapper() {
+  return (
+    <>
+      <main className="flex-1">
+        <HeroSection />
+        <FeaturesSection />
+        <AboutSection />
+        <TestimonialsSection />
+        <PricingSection />
+      </main>
+      <Footer />
+    </>
+  )
+} 
