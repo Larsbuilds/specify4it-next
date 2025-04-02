f4f073cb8b53fb985182c0972e4d9ff3f29f0361
Larsbuilds <larstischer@icloud.com>
2025-04-02 16:06:01 +0300

    fix: add i18n configuration - Create i18n.ts - Update next.config.js to use correct i18n path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')()
+const withNextIntl = require('next-intl/plugin')(
+  './i18n.ts'
+);
 
 const nextConfig = {
   reactStrictMode: true,
